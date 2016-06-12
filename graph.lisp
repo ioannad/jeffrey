c3da990b58b4de419a506853fc4a325d2b4241a1
Ioanna M. Dimitriou H <ioanna.m.dimitriou@gmail.com>
2016-06-12 12:39:00 +0200

    added name LaTeX (statement) and references to the nodes. changed make-node to take these three things as arguments. added verbosity to print-object, add-edge and add-parent.

--- graph.lisp
+++ graph.lisp
@@ -1,16 +1,27 @@
 (in-package :jeffrey.graph)
 
+(defvar *graph* (make-hash-table :test 'equal)
+  "The graph structure may be saved in this universal variable.")
+
+;; The graph structure consists of nodes, whose names have type
+;; integer, though they should be natural numbers.
+
 (defstruct (node (:constructor make-node%))
-  (edges nil :type list)
-  (parents nil :type list)
+  (name       -23   :type integer) ; A node must have a name.
+  (edges      nil   :type list)   
+  (parents    nil   :type list)
+  (LaTeX      ""    :type string)
+  (references ""    :type string) 
   (attributes))
 
-(defun make-node ()
-  (make-node%))
+(defun make-node (name LaTeX-statement references)
+  (check-type name            integer)
+  (check-type LaTeX-statement string)
+  (make-node% :name name :LaTeX LaTeX-statement :references references))
 
 (defstruct (edge (:constructor make-edge%))
   (destination nil :type node)
-  (relation T :type symbol)
+  (relation    T   :type symbol)
   (attributes))
 
 (defun make-edge (destination relation)
@@ -21,7 +32,7 @@
 (defun add-edge (node edge)
   (check-type edge edge)
   (assert (not (member edge #1=(node-edges node))) ()
-	  "EDGE is already an edge of NODE.")
+	  "EDGE ~a is already an edge of NODE ~a." edge node)
   (assert (not (and (member #2=(edge-destination edge)
 			    (node-parents node))
 		    (edge-relation edge))) ()
@@ -31,14 +42,18 @@
 
 (defun add-parent (node parent)
   (check-type parent node)
+  (check-type node   node)
   (assert (not (member parent #1=(node-parents node))) (parent)
-	  "~a is already a parent of NODE." parent)
+	  "~a is already a parent of ~a." parent node)
+  (assert (not (equal node parent)) (parent)
+	  "Parent and node are the same: ~a." parent)
   (push parent #1#))
 
 ;; Prevent stack overflow on printing graph structures
 
 (defmethod print-object ((node node) stream)
   (print-unreadable-object (node stream :type t :identity t)
-    (format stream "~a edges, ~a parents"
+    (format stream "~a: ~a edges, ~a parents "
+	    (node-name node)
 	    (length (node-edges node))
 	    (length (node-parents node)))))
