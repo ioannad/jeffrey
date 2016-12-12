a81e8bfb1ba89c3b32e905f6c1a97ea8b2f514e3
ioa <ioa@nna.fendersons>
2016-12-12 03:06:35 +0100

    changed location of fancy-labels/

--- make-fancy-labels.sh
+++ make-fancy-labels.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-for i in diagrams/fancy-labels/*.tex
+for i in fancy-labels/*.tex
 do 
-    pdflatex -output-directory diagrams/fancy-labels "$i"
+    pdflatex -output-directory fancy-labels "$i"
     convert -density 300 -trim "${i/.tex/}".pdf -quality 100 "${i/.tex/}".png
 done
 
-rm diagrams/fancy-labels/*.log
-rm diagrams/fancy-labels/*.aux
+rm fancy-labels/*.log
+rm fancy-labels/*.aux
